fb7b72d7ee07ebd8c99659cbacf2d199e9be5b69
Christian Lotito <lotitochristian@gmail.com>
2026-09-18 10:49:54 +0200

    Aggiunta classe Persona su Master

---
+++ src/Persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
